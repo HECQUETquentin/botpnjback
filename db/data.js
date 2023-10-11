cc2c4da9ae7480de041fd31da19c9302b96dffa4
Quentin Hecquet <quentin.hecquet@outlook.fr>
2023-10-11 15:39:42 +0200

    contenu

---
+++ db/data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+        id: 0,
+        question: "Quelle est la femelle du hamster ?",
+        answer: "L'Amsterdam"
+    },
+    {
+        id: 1,
+        question: "Que dit un oignon quand il se cogne ?",
+        answer: "Aïe"
+    },
+    {
+        id: 2,
+        question: "Bonjour Henry, comment s'est passé la semaine dernière ?",
+        answer: "Bonjour, ça s'est bien passé. On a fini le dossier de Mr Dupont et on a commencé le dossier de François Damien. "
+    },
+    {
+        id: 3,
+        question: "Bonjour Henry, je vais aller prendre une pause. Est-ce que tu veux un café ?  ",
+        answer: "Bonjour, j'aimerai bien un café long."
+    }
+]
+
+module.exports = data;
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const routes = require("./routes/routes")
+const cors = require('cors')
+
+app.use(express.json())
+app.use(cors())
+
+app.use('/api/v1', routes)
+
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + "/view/404.html")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
