4cb2fc9546fe635a42fe3ff69aa4f8ffc318339f
Quentin Hecquet <quentin.hecquet@outlook.fr>
2023-10-12 09:28:31 +0200

    update db

--- db/data.js
+++ db/data.js
@@ -13,11 +13,13 @@ const data = [
     },
     {
         id: 2,
+        value: "semaine",
         question: "Bonjour Henry, comment s'est passé la semaine dernière ?",
         answer: "Bonjour, ça s'est bien passé. On a fini le dossier de Mr Dupont et on a commencé le dossier de François Damien. "
     },
     {
         id: 3,
+        value: "café",
         question: "Bonjour Henry, je vais aller prendre une pause. Est-ce que tu veux un café ?  ",
         answer: "Bonjour, j'aimerai bien un café long."
     }
